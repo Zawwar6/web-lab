3f7861bf7bbf4f5624c189f393d4eb6ced937034
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2025-12-04 02:17:53 +0500

    Hello

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ function toggleMenu(){
   let menu = document.getElementById("mobileMenu");
   menu.style.right = menu.style.right === "0px" ? "-250px" : "0px";
 }
+ const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: { slidesPerView: 1, spaceBetween: 10 },
+        640: { slidesPerView: 2, spaceBetween: 20 },
+        1024: { slidesPerView: 3, spaceBetween: 30 },
+        1200: { slidesPerView: 4, spaceBetween: 30 },
+      },
+    });
